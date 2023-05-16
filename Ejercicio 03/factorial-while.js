0b9b8146800f381a160f3cb6fcd161c82d250116
Alejandro <131410354+vSheigramARG@users.noreply.github.com>
2023-05-16 15:06:41 -0300

    Update factorial-while.js

--- Ejercicio 03/factorial-while.js
+++ Ejercicio 03/factorial-while.js
@@ -1,12 +1,7 @@
-function factorialize(num) {
-    var result = num;
-    if (num === 0 || num === 1) 
-        return 1; 
-    while (num > 1) { 
-        num--;
-        result *= num;
-    }
-    return result;
+let factorial = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
 }
-
-console.log(factorialize(10));
+console.log(factorial)
