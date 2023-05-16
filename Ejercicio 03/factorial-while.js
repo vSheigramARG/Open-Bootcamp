d2885688438ff6df9d4b955727a4026ce3b2238e
Alejandro <131410354+vSheigramARG@users.noreply.github.com>
2023-05-16 15:05:00 -0300

    Add files via upload

---
+++ Ejercicio 03/factorial-while.js
@@ -0,0 +1,12 @@
+function factorialize(num) {
+    var result = num;
+    if (num === 0 || num === 1) 
+        return 1; 
+    while (num > 1) { 
+        num--;
+        result *= num;
+    }
+    return result;
+}
+
+console.log(factorialize(10));
