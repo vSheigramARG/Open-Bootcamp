803e1ac0f666dab5fcaa9e474ab84985538242e5
Alejandro <131410354+vSheigramARG@users.noreply.github.com>
2023-05-29 16:34:09 -0300

    Update ejercicio_numbers.js

--- ejercicio Numbers/ejercicio_numbers.js
+++ ejercicio Numbers/ejercicio_numbers.js
@@ -1,5 +1,7 @@
-alturaEnCm = 178;
-alturaEnMts = 1.78;
-peso = 82.5;
-alturaEnMtsRedondeada = (Math.round(alturaEnMts));
-pesoEnMtsRedondeadaHaciaAbajo = (Math.floor(peso));
+const alturaEnCm = 178;
+const alturaEnMts = 1.78;
+const peso = 82.5;
+const alturaEnMtsRedondeada = (Math.ceil(alturaEnMts));
+const pesoEnMtsRedondeadaHaciaAbajo = (Math.floor(peso));
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
