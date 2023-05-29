d2244cba8bdf966d86db0cf61ead2c572bd28784
Alejandro <131410354+vSheigramARG@users.noreply.github.com>
2023-05-29 13:23:36 -0300

    Add files via upload

---
+++ ejercicio Strings/ejercicio1.js
@@ -0,0 +1,12 @@
+nombre = "Alejandro";
+apellido = "Espasandín";
+estudiante = (nombre + " " + apellido);
+estudianteMayus = estudiante.toUpperCase();
+estudianteMinus = estudiante.toLowerCase();
+
+const longitudEstudiante = estudiante.length;
+
+const primeraLetra = nombre.charAt(nombre.length[0]);
+const ultimaLetra = apellido.charAt(apellido.length -1);
+
+const estudianteSinEspacios = estudiante.split(" ").join("");
