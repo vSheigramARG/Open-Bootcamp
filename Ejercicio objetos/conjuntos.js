16400d4fa5c47d436167e26a6556380914044760
Alejandro <131410354+vSheigramARG@users.noreply.github.com>
2023-06-12 15:26:12 -0300

    Add files via upload

---
+++ Ejercicio objetos/conjuntos.js
@@ -0,0 +1,7 @@
+const nombresFamilia = ["Alejandra", "Alejandro", "Valentina", "Lua", "Nya"];
+
+const setNombresFamilia = new Set(nombresFamilia);
+
+setNombresFamilia.add("Alejandro");
+
+setNombresFamilia.add("Javascript");
